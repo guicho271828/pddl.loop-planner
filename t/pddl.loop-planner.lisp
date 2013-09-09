32af0235793a912b042ed4129fb388afda83e5e4
Masataro Asai <guicho2.71828@gmail.com>
2013-09-09 15:13:39 +0900

    migration to lparallel done

--- t/pddl.loop-planner.lisp
+++ t/pddl.loop-planner.lisp
@@ -14,6 +14,7 @@
         :guicho-utilities.threading
         :guicho-red-black-tree
         :bordeaux-threads
+        :lparallel
 	:pddl
         :pddl.loop-detection
         :pddl.loop-planner
@@ -65,12 +66,12 @@
                  tmpdir))
               (progn
                 (format t "~3%Exploiting loopable steady-states from the movements.
-It takes a long time (> around 4 min), please wait...")
+It takes a long time (> around 4 min), please wait...~%")
                 (sleep 2)
                 (time (exploit-loopable-steady-states
                        movements
                        (exploit-steady-states movements)
-                       :verbose :modest)))))))
+                       :verbose nil)))))))
 
 (defparameter problem-pathnames
   (remove-if-not (lambda (path)
@@ -118,95 +119,85 @@ It takes a long time (> around 4 min), please wait...")
     (join-thread t2)
     (pass "~%finished!")))
 
-(defun sleep-and-say-hi-limited ()
-  (with-limited-cores ()
-    (sleep (random 1.5))
-    (with-lock-held (*print-lock*)
-      (format *shared-output* "~%Hi! I am ~a !" (current-thread)))
-    t))
-
-(test (limited-core)
-  (let ((threads
+(test (future-based-threading)
+  (let ((futures
          (iter (for i below 10)
-               (for th = (make-thread #'sleep-and-say-hi-limited
-                                      :name (format nil "Test limited-core thread ~a" i)))
-               (print th)
-               (collect th))))
-    (dolist (th threads)
-      (join-thread th)
-      (format t "~%thread ~a joined!" (thread-name th))
+               (for f = (future (sleep-and-say-hi)))
+               (print f)
+               (collect f))))
+    (dolist (f futures)
+      (force f)
+      (format t "~%thread ~a joined!" f)
       (pass))))
 
+(defun %shorthand-pathname (path &optional (within 20))
+  (let* ((str (concatenate 'string (pathname-name path) "." (pathname-type path)))
+         (len (length str))
+         (back (clamp (- len within) 0 within)))
+    (concatenate 'string "..." (subseq str back len))))
+
 (defvar *result-lock* (make-lock "Result lock"))
-(defvar loop-plan-results
-  (make-hash-table))
-(defun test-problem-and-get-plan ()
-  (let ((ppath (random-elt problem-pathnames)))
-    (with-limited-cores (300)
-      (with-lock-held (*print-lock*)
-        (format *shared-output*
-                "~%Thread ~a Solving ~a ..."
-                (thread-name (current-thread)) (pathname-name ppath)))
-      (let* ((*domain* cell-assembly)
-             (*problem* (%make-problem ppath))
-             (plans
-              (mapcar
-               #'%make-plan
-               (test-problem ppath domain-pathname :stream nil))))
-        
-        (with-lock-held (*result-lock*)
-          (setf (gethash *problem* loop-plan-results) plans))
-        
-        (dolist (plan plans)
-          (let* ((seq (sequencial-length plan))
-                 (par (parallel-length plan))
-                 (base-count (count-objects *problem* base-type))
-                 (time-per-base (/ par base-count)))
-            
-            ;; (with-lock-held (*test-lock*)
-            ;;   (is (<= par seq))
-            ;;   (is (< 1 base-count)))
-            (with-lock-held (*print-lock*)
-              (terpri *shared-output*)
-              (pprint-logical-block (*shared-output*
-                                     nil
-                                     :per-line-prefix
-                                     (thread-name (current-thread)))
-                (format
-                 *shared-output* "~%~{~10<~a~>~}~%~{~10<~5,2f~>~}"
-                 '(seq. par. base time/base seq./par.)
-                 (list seq par base-count time-per-base (/ seq par)))))
-            (%rb-queue-safe time-per-base (list plan *problem*))))))))
+(defvar loop-plan-results (make-hash-table))
+
+(defvar *workers* nil)
+(defun test-problem-and-get-plan (ppath)
+  (with-lock-held (*print-lock*)
+    (format *shared-output*
+            "~%Thread ~a Solving ~a ..."
+            (current-thread)
+            (%shorthand-pathname ppath)))
+  (let* ((*domain* cell-assembly)
+         (*problem* (%make-problem ppath))
+         (plans
+          (mapcar
+           #'%make-plan
+           (test-problem ppath domain-pathname :stream nil))))
+
+    (with-lock-held (*result-lock*)
+      (setf (gethash *problem* loop-plan-results) plans))
+
+    (dolist (plan plans)
+      (let* ((seq (sequencial-length plan))
+             (par (parallel-length plan))
+             (base-count (count-objects *problem* base-type))
+             (time-per-base (/ par base-count)))
+
+        ;; (with-lock-held (*test-lock*)
+        ;;   (is (<= par seq))
+        ;;   (is (< 1 base-count)))
+        (with-lock-held (*print-lock*)
+          (terpri *shared-output*)
+          (pprint-logical-block (*shared-output*
+                                 nil
+                                 :per-line-prefix
+                                 (princ-to-string (current-thread)))
+            (format
+             *shared-output* "~%~{~10<~a~>~}~%~{~10<~5,2f~>~}"
+             '(seq. par. base time/base seq./par.)
+             (list seq par base-count time-per-base (/ seq par)))))
+        (%rb-queue-safe time-per-base (list plan *problem*))))))
 
 (test (get-plan-1)
   (finishes
-    (test-problem-and-get-plan))
+    (test-problem-and-get-plan (random-elt problem-pathnames))
   (format t "~2%
 ***************************************~3%
   For more parallelized results, run ~w .~3%
 ***************************************~2%"
           '(progn
             (in-package :pddl.loop-planner-test)
-            (get-plans))))
+            (get-plans)))))
 
 (defun get-plans (&optional (howmany 40))
-  (let (threads (low 0) (high howmany))
-    (restart-return ((destroy-all-threads
-                      (lambda ()
-                        (dolist (th threads)
-                          (when (thread-alive-p th)
-                            (destroy-thread th)))
-                        nil)))
+  (let ((total 0))
+    (restart-return ((finish (lambda () nil)))
       (do-restart ((run-20-times-more
-                    (lambda ()
-                      (incf low 20)
-                      (incf high 20))))
-        (iter (for i from low below high)
-              (push (make-thread
-                     #'test-problem-and-get-plan
-                     :name (format nil "~4@<#~a~>" i))
-                    threads))
-        (mapc #'join-thread threads)
+                    (lambda () (setf howmany 20))))
+        (incf total howmany)
+        (pdotimes (i howmany)
+          (test-problem-and-get-plan
+           (random-elt problem-pathnames)))
+        
         (multiple-value-bind (content value)
             (rb-minimum parallelized-loop-plan-results)
           (error "what to do next?
@@ -214,5 +205,5 @@ problems searched          = ~a
 current minumum time/base  = ~5,2f
 corresponding plan,problem =
   ~w"
-                 high value content))))))
+                 total value content))))))
 
