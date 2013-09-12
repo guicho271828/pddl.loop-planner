844e6615955bfad06ea45ee7d97d9b0b736343c2
Masataro Asai <guicho2.71828@gmail.com>
2013-09-13 00:41:14 +0900

    refactoring: code movements

--- t/pddl.loop-planner.lisp
+++ t/pddl.loop-planner.lisp
@@ -35,6 +35,37 @@
 (def-suite :pddl.loop-planner :in :pddl)
 (in-suite :pddl.loop-planner)
 
+(defun sleep-and-say-hi ()
+  (sleep (random 1.5))
+  (with-lock-held (*print-lock*)
+    (format *shared-output* "~%Hi! I am ~a !" (current-thread)))
+  t)
+
+;;;; parallelization test
+
+(test (test-threaded-test)
+  (let ((t1 (make-thread #'sleep-and-say-hi))
+        (t2 (make-thread #'sleep-and-say-hi)))
+    (print t1)
+    (print t2)
+    (join-thread t1)
+    (join-thread t2)
+    (pass "~%finished!")))
+
+(test (future-based-threading)
+  (let ((futures
+         (iter (for i below 10)
+               (for f = (future (sleep-and-say-hi)))
+               (print f)
+               (collect f))))
+    (dolist (f futures)
+      (force f)
+      (format t "~%thread ~w joined!" (thread-name f))
+      (pass))))
+
+;;;; main tests
+
+
 (defvar domain-directory
   (asdf:system-relative-pathname
    :pddl.loop-planner
@@ -48,8 +79,9 @@
 (defparameter tmpdir
   (merge-pathnames (string-downcase (gensym "cell-assebly")) #p"/tmp/"))
 
-(pprint-logical-block (*standard-output* nil
-                                       :per-line-prefix ";Test Preparation process: ")
+(pprint-logical-block (*standard-output*
+                       nil
+                       :per-line-prefix ";Test Preparation process: ")
   (let ((unit-problem cell-assembly-model2a-1)
         (unit-plan cell-assembly-model2a-1-6))
     (let ((schedule (reschedule unit-plan
@@ -106,32 +138,6 @@ It takes a long time (> around 4 min), please wait...~%")
                   x
                   parallelized-loop-plan-results))))))
 
-(defun sleep-and-say-hi ()
-  (sleep (random 1.5))
-  (with-lock-held (*print-lock*)
-    (format *shared-output* "~%Hi! I am ~a !" (current-thread)))
-  t)
-
-(test (test-threaded-test)
-  (let ((t1 (make-thread #'sleep-and-say-hi))
-        (t2 (make-thread #'sleep-and-say-hi)))
-    (print t1)
-    (print t2)
-    (join-thread t1)
-    (join-thread t2)
-    (pass "~%finished!")))
-
-(test (future-based-threading)
-  (let ((futures
-         (iter (for i below 10)
-               (for f = (future (sleep-and-say-hi)))
-               (print f)
-               (collect f))))
-    (dolist (f futures)
-      (force f)
-      (format t "~%thread ~a joined!" f)
-      (pass))))
-
 (defun %shorthand-pathname (path &optional (within 20))
   (let* ((str (concatenate 'string (pathname-name path) "." (pathname-type path)))
          (len (length str))
@@ -142,7 +148,10 @@ It takes a long time (> around 4 min), please wait...~%")
 (defvar *result-lock* (make-lock "Result lock"))
 (defvar loop-plan-results (make-hash-table))
 
-(defvar *workers* nil)
+(defun clean-results ()
+  (clrhash loop-plan-results)
+  (setf parallelized-loop-plan-results (leaf)))
+
 (defun test-problem-and-get-plan (ppath &key
                                   (memory 200000000)
                                   (time-limit 15))
