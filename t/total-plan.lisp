adb11d0d023b28acbc53a262214bb1b1ff870454
Masataro Asai <guicho2.71828@gmail.com>
2013-10-06 00:00:47 +0900

    fix: standard-output > (config :daily FILE)

--- t/total-plan.lisp
+++ t/total-plan.lisp
@@ -13,27 +13,32 @@
        :directory tmpdir)
       (run `(rm -r ,tmpdir)))))
 
-(defun benchmark ()
-  (let ((dropbox (merge-pathnames #p"Dropbox/" (user-homedir-pathname))))
-    (with-open-file (*standard-output*
-                     (merge-pathnames "loop-planner.log" dropbox)
-                     :direction :output
-                     :if-does-not-exist :create
-                     :if-exists :append)
-      (iter (for plan in (list cell-assembly-model2b-1-6
-                               cell-assembly-model2a-1-6
-                               cell-assembly-model3a-1-5
-                               cell-assembly-model3b-1-5
-                               cell-assembly-model3c-1-8))
+(defvar *log-dir*
+  (merge-pathnames
+   #p"Dropbox/loop-planner/"
+   (user-homedir-pathname)))
+(ensure-directories-exist *log-dir*)
 
-            (log:info "start planning" plan)
-            (iter (for i in '(4 16 64 256 1024))
-                  (log:info "start planning ~a base plan" i)
-                  (log:info "result:"
-                            (multiple-value-list 
-                             (solve-many-problems
-                              i plan 'b-0
-                              :howmany 40 :time-limit 120
-                              :base-limit 3 :interactive nil
-                              :directory dropbox))))))))
+(defvar *log-name*
+  (merge-pathnames #p"loop-planner" *log-dir*))
+
+(log:config :daily *log-name*)
+  
+(defun benchmark ()
+  (iter (for plan in (list cell-assembly-model2b-1-6
+                           cell-assembly-model2a-1-6
+                           cell-assembly-model3a-1-5
+                           cell-assembly-model3b-1-5
+                           cell-assembly-model3c-1-8))
+
+        (log:info "start planning" plan)
+        (iter (for i in '(4 16 64 256 1024))
+              (log:info "start planning ~a base plan" i)
+              (log:info "result:"
+                        (multiple-value-list 
+                         (solve-many-problems
+                          i plan 'b-0
+                          :howmany 40 :time-limit 120
+                          :base-limit 3 :interactive nil
+                          :directory dropbox))))))
 
