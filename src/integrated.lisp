53196fe9c76831af69681b569b335812c44886de
Masataro Asai <guicho2.71828@gmail.com>
2013-10-05 11:33:29 +0900

    added the integrated function

---
+++ src/integrated.lisp
@@ -0,0 +1,25 @@
+(in-package :pddl.loop-planner)
+(cl-syntax:use-syntax :annot)
+
+@export
+(defun solve-many-problem (basenum unit-plan unit-object
+                           &rest rest
+                           &key
+                           (howmany 40)
+                           (memory 200000000)
+                           (time-limit 15)
+                           (base-limit MOST-POSITIVE-FIXNUM)
+                           (handler #'my-handler))
+  (declare (ignorable howmany memory time-limit base-limit handler))
+  (multiple-value-bind (result base-type)
+      (apply #'exploit-and-solve-loop-problems
+             unit-plan unit-object :lazy t rest)
+    (apply #'build-total-plan
+           basenum
+           unit-plan
+           base-type
+           (first result))
+    ;; (multiple-value-bind (initial-actions actions total-problem all-bases)
+    
+    ;;   )
+    ))
--- t/total-plan.lisp
+++ t/total-plan.lisp
@@ -5,15 +5,9 @@
 
 (test total-plan
   (finishes
-    (multiple-value-bind (result base-type)
-        (exploit-and-solve-loop-problems
-         cell-assembly-model2a-1-6 'b-0
-         :howmany 1 :lazy t :base-limit 3)
-      (multiple-value-bind (actions total-problem all-bases)
-          (apply #'build-total-plan
-                 2
-                 cell-assembly-model2a-1-6
-                 base-type
-                 (first result))
-        ))))
+    (solve-many-problem
+     2
+     cell-assembly-model2a-1-6
+     'b-0
+     :howmany 3 :time-limit 20 :base-limit 5)))
 
