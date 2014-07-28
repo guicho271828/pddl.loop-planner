9656df5fb29bdf785764e854d1fcab6912707319
Masataro Asai <guicho2.71828@gmail.com>
2014-07-28 11:38:08 +0900

    changed benchmark.lisp

--- benchmark/benchmark.lisp
+++ benchmark/benchmark.lisp
@@ -8,17 +8,16 @@
 
 (let ((*domain* pddl.instances:cell-assembly-eachparts)
       (*problem* pddl.instances:cell-assembly-model2a-each-1))
-  (format t "~{~&~a~%~}"
-          (multiple-value-list
-           (exploit-loop-problems
-            (pddl-plan :path "/mnt/video/guicho/repos/lisp/pddl/data/model2a-each/p0001.plan.1")
-            (list (object *problem* :b-0)
-                  (object *problem* :part-a-0)
-                  (object *problem* :part-b-0)
-                  (object *problem* :part-c-0))
-            #'evaluate-loop-problem
-            :verbose t
-            :timeout 30))))
+  (time
+   (exploit-loop-problems
+    (pddl-plan :path "/mnt/video/guicho/repos/lisp/pddl/data/model2a-each/p0001.plan.1")
+    (list (object *problem* :b-0)
+          (object *problem* :part-a-0)
+          (object *problem* :part-b-0)
+          (object *problem* :part-c-0))
+    #'evaluate-loop-problem
+    :verbose t
+    :timeout 30)))
 
 pddl.instances:cell-assembly-2a2b-mixed-each-1
 pddl.instances:cell-assembly-model2a-each-1
