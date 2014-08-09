efb83bdb2f6aafdcd7b67fc135f06b50cdb348d4
Masataro Asai <guicho2.71828@gmail.com>
2014-08-09 21:14:44 +0900

    move component into test-problem

--- t/test-problem.lisp
+++ t/test-problem.lisp
@@ -39,3 +39,14 @@
         #'evaluate-loop-problem
         :verbose t
         :timeout 2)))))
+
+(test elp-with-e-with-component
+  (finishes
+    (let ((*domain* assemble) (*problem* assemblep))
+      (describe
+       (exploit-loop-problems
+        (pddl-plan :actions (parse-plan +assembleplan+))
+        (list (object *problem* :p1)
+              (object *problem* :pa11)
+              (object *problem* :pa12))
+        #'evaluate-loop-problem)))))
